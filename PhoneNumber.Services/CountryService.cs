d358176e71368c95a65d9130a27089f99937fcb5
Dominic James <30402312+dom3691@users.noreply.github.com>
2024-01-09 10:24:56 +0100

    Added Services

---
+++ PhoneNumber.Services/CountryService.cs
@@ -0,0 +1,26 @@
+﻿using PhoneNumber.Domain.Entities;
+using PhoneNumber.Domain.Interfaces.Data;
+using PhoneNumber.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumber.Services
+{
+    public class CountryService : ICountryService
+    {
+        private readonly ICountryRepository countryRepository;
+
+        public CountryService(ICountryRepository countryRepository)
+        {
+            this.countryRepository = countryRepository;
+        }
+
+        public Country GetCountryByCode(int countryCode)
+        {
+            return countryRepository.GetCountryByCode(countryCode);
+        }
+    }
+}
---
+++ PhoneNumber.Services/PhoneService.cs
@@ -0,0 +1,55 @@
+﻿using PhoneNumber.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumber.Services
+{
+    public class PhoneInfoService : IPhoneInfoService
+    {
+        private readonly ICountryService countryService;
+        private readonly ICountryDetailService countryDetailService;
+
+        public PhoneInfoService(ICountryService countryService, ICountryDetailService countryDetailService)
+        {
+            this.countryService = countryService;
+            this.countryDetailService = countryDetailService;
+        }
+
+        public object GetCountryInfo(string phoneNumber)
+        {
+            // Extract country code from the phone number (assuming it's the first digits)
+            if (int.TryParse(phoneNumber.Substring(0, 3), out int countryCode))
+            {
+                var country = countryService.GetCountryByCode(countryCode);
+
+                if (country != null)
+                {
+                    var countryDetails = countryDetailService.GetCountryDetailsByCountryId(country.Id);
+
+                    var result = new
+                    {
+                        number = phoneNumber,
+                        country = new
+                        {
+                            countryCode = country.CountryCode,
+                            name = country.Name,
+                            countryIso = country.CountryIso,
+                            countryDetails = countryDetails.Select(cd => new
+                            {
+                                operatorName = cd.Operator,
+                                operatorCode = cd.OperatorCode
+                            }).ToList()
+                        }
+                    };
+
+                    return result;
+                }
+            }
+
+            return null;
+        }
+    }
+}
