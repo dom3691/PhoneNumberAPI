55a42dbe14cc5c742fd5cfb39b2ec859436f11cd
Dominic James <30402312+dom3691@users.noreply.github.com>
2024-01-09 10:32:51 +0100

    Addedd Dependency Injection to Program.Cs and installed SqlServer and inMemory Entity Framewrk

--- PhoneNumber.Services/CountryDetailService.cs
+++ PhoneNumber.Services/CountryDetailService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PhoneNumber.Domain.Entities;
+using PhoneNumber.Domain.Interfaces.Data;
+using PhoneNumber.Domain.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace PhoneNumber.Services
 {
-    internal class CountryDetailService
+    public class CountryDetailService : ICountryDetailService
     {
+        private readonly ICountryDetailsRepository countryDetailRepository;
+
+        public CountryDetailService(ICountryDetailsRepository countryDetailRepository)
+        {
+            this.countryDetailRepository = countryDetailRepository;
+        }
+
+        public List<CountryDetails> GetCountryDetailsByCountryId(int countryId)
+        {
+            return countryDetailRepository.GetCountryDetailsByCountryId(countryId);
+        }
     }
 }
--- PhoneNumber/Program.cs
+++ PhoneNumber/Program.cs
@@ -1,3 +1,8 @@
+using PhoneNumber.Data;
+using PhoneNumber.Domain.Interfaces.Data;
+using PhoneNumber.Domain.Interfaces.Services;
+using PhoneNumber.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPhoneInfoService, PhoneInfoService>();
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<ICountryDetailService, CountryDetailService>();
+builder.Services.AddTransient<ICountryRepository, CountryRepository>();
+builder.Services.AddTransient<ICountryDetailsRepository, CountryDetailRepository>();
 
 var app = builder.Build();
 
